cd64fe10b441b7ce36d647b5fcb63f3edbfe2d16
ronnie <rvpostha15@yahoo.com>
2023-02-15 20:56:50 -0500

    test2

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2023_02_15_194616) do
+ActiveRecord::Schema.define(version: 2023_02_15_204017) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "property_id"
